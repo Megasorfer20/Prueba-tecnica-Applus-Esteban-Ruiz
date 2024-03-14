39cd2dfb68642c1d4fca0af7eabdf48497b047e4
Esteban Dario Ruiz <edrp.campus15@gmail.com>
2024-03-14 18:31:12 -0500

    feat: :sparkles: Coneccion a base de datos

---
+++ Creacion-Cruds/backend/database/conection.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+require_once "database.php";
+
+abstract class Conection
+{
+    protected $dbcnx;
+    public function __construct()
+    {
+        $this->dbcnx = new PDO(DBTYPE . ":host=" . DBHOST.";dbname=".DBNAME, DBUSER,DBPASSWORD,[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    }
+}
+
+?>
---
+++ Creacion-Cruds/backend/database/database.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+$env = parse_ini_file(".env");
+
+if (!defined("DBTYPE")) {
+    define("DBTYPE", $env['DBTYPE']);
+}
+if (!defined("DBHOST")) {
+    define("DBHOST", $env['DBHOST']);
+}
+if (!defined("DBNAME")) {
+    define("DBNAME", $env['DBNAME']);
+}
+if (!defined("DBUSER")) {
+    define("DBUSER", $env['DBUSER']);
+}
+if (!defined("DBPASSWORD")) {
+    define("DBPASSWORD", $env['DBPASSWORD']);
+}
